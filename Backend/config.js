c7ca8d321a9cfd5b21fb21adc96a14fb711ecc1f
Carl Sebastian Marnewick <91884865+BubbleGumWarior@users.noreply.github.com>
2024-11-06 12:56:55 +0200

    Update config.js

--- Backend/config.js
+++ Backend/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  localIP: 'dungeonlair.ddns.net', // No-IP hostname
-  JWT_SECRET: 'DungeonLairIsAwesome',
+  localIP: 'ip', // No-IP hostname
+  JWT_SECRET: 'HWT token that you make yourself',
   DB_HOST: 'localhost', // Ensure this is correct
-  DB_PORT: 5432,
-  DB_NAME: 'dungeonlair',
-  DB_USER: 'postgres',
-  DB_PASSWORD: 'admin'
+  DB_PORT: port,
+  DB_NAME: 'dbname',
+  DB_USER: 'db admin user',
+  DB_PASSWORD: 'db admin user password'
 };
